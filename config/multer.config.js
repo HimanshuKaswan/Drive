a21ed36f460a2ccfd64a3ddce6cda6f739925812
Himanshu <Himanshu>
2025-05-06 18:38:51 +0530

    missing code added.

--- config/multer.config.js
+++ config/multer.config.js
@@ -2,6 +2,7 @@ const multer = require('multer')
 const firebaseStorage = require('multer-firebase-storage')
 const firebase = require('./firebase.config')
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG)
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 const storage = firebaseStorage({
     credentials: firebase.credential.cert(serviceAccount),
